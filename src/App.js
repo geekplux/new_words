7731e6c29f7f3cd27cbe3c5f4479bc728c7ddcd5
geekplux <geekplux@gmail.com>
2019-01-14 19:44:25 +0800

    feat: name and desc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const Char = props => <span>{props.c}</span>;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wordName: '新名词',
+      wordDesc: '新名词解释',
+      hideInput: false
+    };
+  }
+  onNameInput = event => {
+    this.setState({
+      wordName: event.target.value
+    });
+  };
+  onDescInput = event => {
+    this.setState({
+      wordDesc: event.target.value
+    });
+  };
+
+  onClick = event => {
+    this.setState({ hideInput: !this.state.hideInput });
+  };
+
   render() {
+    const { hideInput, wordName, wordDesc } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p className="title">新名词生成器</p>
+        <div className="name">
+          {wordName}
+        </div>
+        {hideInput ? null : (
+          <input
+            className="nameInput"
+            type="text"
+            placeholder="*只能输入中文"
+            value={wordName}
+            onChange={this.onNameInput}
+          />
+        )}
+        <p className="nameNormal">{wordName}：</p>
+        <p className="desc">{wordDesc}</p>
+        {hideInput ? null : (
+          <textarea
+            className="descInput"
+            placeholder="输入新名词的解释"
+            value={wordDesc}
+            onChange={this.onDescInput}
+          />
+        )}
+        <button className="btn" onClick={this.onClick}>
+          {hideInput ? '修改' : '完成'}
+        </button>
+        <a className="author" href="https://geekplux.com">
+          © GeekPlux
+        </a>
       </div>
     );
   }
