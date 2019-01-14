18b6603ae3ec882a998bab92c48aa6c91e07353c
geekplux <geekplux@gmail.com>
2019-01-14 20:28:41 +0800

    feat: add style

--- src/App.js
+++ src/App.js
@@ -32,37 +32,43 @@ class App extends Component {
 
     return (
       <div className="App">
-        <p className="title">新名词生成器</p>
-        <div className="name">
-          {wordName &&
-            wordName.length &&
-            wordName.split('').map((c, i) => <Char key={i} c={c} />)}
+        <div className="container">
+          <h1 className="title">新名词生成器</h1>
+          <div className="nameChar">
+            {wordName &&
+              wordName.length &&
+              wordName.split('').map((c, i) => <Char key={i} c={c} />)}
+          </div>
+          {hideInput ? null : (
+            <input
+              className="nameInput"
+              type="text"
+              placeholder="*最多十个字符"
+              maxLength={10}
+              value={wordName}
+              onChange={this.onNameInput}
+            />
+          )}
+          <p className="name">{wordName}：</p>
+          <p className="desc">{wordDesc}</p>
+          {hideInput ? null : (
+            <textarea
+              className="descInput"
+              placeholder="输入新名词的解释"
+              value={wordDesc}
+              onChange={this.onDescInput}
+            />
+          )}
+          <button className="btn" onClick={this.onClick}>
+            {hideInput ? '修改' : '完成'}
+          </button>
+          <p className="author">
+            © made by
+            <a href="https://geekplux.com">
+              GeekPlux
+            </a>
+          </p>
         </div>
-        {hideInput ? null : (
-          <input
-            className="nameInput"
-            type="text"
-            placeholder="*只能输入中文"
-            value={wordName}
-            onChange={this.onNameInput}
-          />
-        )}
-        <p className="nameNormal">{wordName}：</p>
-        <p className="desc">{wordDesc}</p>
-        {hideInput ? null : (
-          <textarea
-            className="descInput"
-            placeholder="输入新名词的解释"
-            value={wordDesc}
-            onChange={this.onDescInput}
-          />
-        )}
-        <button className="btn" onClick={this.onClick}>
-          {hideInput ? '修改' : '完成'}
-        </button>
-        <a className="author" href="https://geekplux.com">
-          © GeekPlux
-        </a>
       </div>
     );
   }
