f64d1782aca74626c45705eb130932e3f7a2e30e
geekplux <geekplux@gmail.com>
2019-01-16 22:04:30 +0800

    refactor: nameChar responsive

--- src/components/char/index.js
+++ src/components/char/index.js
@@ -5,14 +5,20 @@ import './style.css';
 class Char extends Component {
   render() {
     return (
-      <span
-        className="char"
-        style={{
-          backgroundImage: `url(${miBg})`
-        }}
-      >
-        {this.props.c}
-      </span>
+      <div className="square">
+        <div className="wrap">
+          <div className="content">
+            <span
+              className="char"
+              style={{
+                backgroundImage: `url(${miBg})`
+              }}
+            >
+              {this.props.c}
+            </span>
+          </div>
+        </div>
+      </div>
     );
   }
 }
