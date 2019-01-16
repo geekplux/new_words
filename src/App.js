1d4f52038aa672cbb7872849548aa8334e906891
luyilin <luyilin12@gmail.com>
2019-01-16 20:51:09 +0800

    feat: add download func

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Char from './components/char';
 import './App.css';
 import pinyinUtil from './pinyinUtil';
+import toImage from 'dom-to-image';
+import download from 'downloadjs';
 
 const pinyin = str => pinyinUtil.getPinyin(str);
 
@@ -29,6 +31,17 @@ class App extends Component {
     this.setState({ hideInput: !this.state.hideInput });
   };
 
+  download = () => {
+    toImage
+      .toBlob(this.word)
+      .then(blob => {
+        download(blob, `${this.state.wordName}.png`, 'image/png')
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
   render() {
     const { hideInput, wordName, wordDesc } = this.state;
 
@@ -36,24 +49,26 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <h1 className="title">新名词生成器</h1>
-          <div className="nameChar">
-            {wordName && wordName.length
-              ? wordName.split('').map((c, i) => <Char key={i} c={c} />)
-              : '*请在下方输入框输入'}
+          <div className="word" ref={(ref) => this.word = ref}>
+            <div className="nameChar">
+              {wordName && wordName.length
+                ? wordName.split('').map((c, i) => <Char key={i} c={c} />)
+                : '*请在下方输入框输入'}
+            </div>
+            <div className="pinyin">{wordName && pinyin(wordName)}</div>
+            {hideInput ? null : (
+              <input
+                className="nameInput"
+                type="text"
+                placeholder="*最多输入十个字符"
+                maxLength={10}
+                value={wordName}
+                onChange={this.onNameInput}
+              />
+            )}
+            <p className="name">{wordName}：</p>
+            <p className="desc">{wordDesc}</p>
           </div>
-          <div className="pinyin">{wordName && pinyin(wordName)}</div>
-          {hideInput ? null : (
-            <input
-              className="nameInput"
-              type="text"
-              placeholder="*最多输入十个字符"
-              maxLength={10}
-              value={wordName}
-              onChange={this.onNameInput}
-            />
-          )}
-          <p className="name">{wordName}：</p>
-          <p className="desc">{wordDesc}</p>
           {hideInput ? null : (
             <textarea
               className="descInput"
@@ -65,6 +80,9 @@ class App extends Component {
           <button className="btn" onClick={this.onClick}>
             {hideInput ? '修改' : '完成'}
           </button>
+          {hideInput &&
+          <button className="btn download-btn" onClick={this.download}>下载图片</button>
+          }
           <p className="author">
             © made by
             <a href="https://geekplux.com">GeekPlux</a>
